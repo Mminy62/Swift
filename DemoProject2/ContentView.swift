6e78dc7aeb93331e0a33d6cc5da2b67d77161e9a
Minyoung Lee <mminy62@gmail.com>
2023-10-11 09:31:50 +0900

    Initial Commit

---
+++ DemoProject2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoProject2
+//
+//  Created by 이민영 on 2023/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoProject2/DemoProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProject2App.swift
+//  DemoProject2
+//
+//  Created by 이민영 on 2023/10/11.
+//
+
+import SwiftUI
+
+@main
+struct DemoProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
