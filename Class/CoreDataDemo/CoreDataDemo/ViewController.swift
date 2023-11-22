e9c6c1dd52188b5f698062921c13084eb2722d29
Minyoung Lee <mminy62@gmail.com>
2023-11-22 16:48:26 +0900

    CoreData 예제 앱 시작
    - 엔티티 설명 만들기(DB테이블과 유사)
    - 사용자 인터페이스 디자인
    - @IBOutlet, @IBAction 연결

---
+++ Class/CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by 이민영 on 2023/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var name: UITextField!
+    @IBOutlet var address: UITextField!
+    @IBOutlet var phone: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func saveContact(_ sender: Any) {
+    }
+    
+}
+
