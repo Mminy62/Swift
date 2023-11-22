e09e4405daae95fbe9d0c100b312d34a2e74f324
Minyoung Lee <mminy62@gmail.com>
2023-11-22 11:27:58 +0900

    SQlite 사용하도록 프로젝트 세팅

---
+++ Class/DatabaseDemo/DatabaseDemo/DatabaseDemo-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+// swift파일에서도 objective-c를 쓸 수 있게 바꾸는 것
+
+#import "FMDB.h"
+
---
+++ Class/DatabaseDemo/DatabaseDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  DatabaseDemo
+//
+//  Created by 이민영 on 2023/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var name: UITextField!
+    @IBOutlet var address: UITextField!
+    @IBOutlet var phone: UITextField!
+    @IBOutlet var status: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func saveContact(_ sender: Any) {
+    }
+    
+    @IBAction func findContact(_ sender: Any) {
+    }
+}
+
