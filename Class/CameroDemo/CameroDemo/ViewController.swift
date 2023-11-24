c55aa2e900f7bbf6af2a3446d00ced9ec3c74435
Minyoung Lee <mminy62@gmail.com>
2023-11-24 14:47:16 +0900

    사진 라이브러리 띄우기

--- Class/CameroDemo/CameroDemo/ViewController.swift
+++ Class/CameroDemo/CameroDemo/ViewController.swift
@@ -6,21 +6,46 @@
 //
 
 import UIKit
+import MobileCoreServices
+import UniformTypeIdentifiers
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     @IBOutlet var imageView: UIImageView!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+
+    // 뒷 부분에서 필요할 Boolean 속성 선언
+    var newMeida: Bool?
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            // Do any additional setup after loading the view.
+        }
 
     @IBAction func useCamera(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.mediaTypes = [UTType.image.identifier]
+            imagePicker.allowsEditing = false
+            
+            self.present(imagePicker, animated: true, completion: nil)
+            
+            newMeida = true
+        }
+        
     }
+    
+//    // 오류가 발생하면 경고 상자를 통해 사용자에게 보고
+//    @objc func image(image: UIImage, )
+//    
+    
     @IBAction func useCameraRoll(_ sender: Any) {
+        
     }
 }
 
