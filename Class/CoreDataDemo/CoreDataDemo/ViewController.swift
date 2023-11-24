601d5891993f5fc2db88aeccf1c64a9aa24dba4a
Minyoung Lee <mminy62@gmail.com>
2023-11-24 14:13:24 +0900

    core data 연습
    core data 생성, 추가, 삭제, 찾기 가능

--- Class/CoreDataDemo/CoreDataDemo/ViewController.swift
+++ Class/CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -13,10 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet var name: UITextField!
     @IBOutlet var address: UITextField!
     @IBOutlet var phone: UITextField!
+    @IBOutlet var status: UILabel!
     
     // 관리 객체 컨텍스트 객체에 대한 참조를 저장할 변수를 선언
+    // filemanager 느낌
     var manageObjectContext: NSManagedObjectContext?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -34,12 +36,61 @@ class ViewController: UIViewController {
                 self.manageObjectContext = container.viewContext
             }
         })
-    }
 
+    }
+    
     @IBAction func saveContact(_ sender: Any) {
+        if let context = self.manageObjectContext, let entityDescription = NSEntityDescription.entity(forEntityName: "Contacts", in: context) {
+
+       let contact = Contacts(entity: entityDescription, insertInto: manageObjectContext)
+
+       contact.name = name.text
+       contact.address = address.text
+       contact.phone = phone.text
+
+       do {
+           try manageObjectContext?.save()
+
+           name.text = ""
+           address.text = ""
+           phone.text = ""
+           status.text = "Contact Added"
+       } catch let error {
+           status.text = error.localizedDescription
+       }
+
+   }
     }
     
     @IBAction func findContact(_ sender: Any) {
+        
+        if let context = self.manageObjectContext, let entityDescription = NSEntityDescription.entity(forEntityName: "Contacts", in: context) {
+
+                    let request: NSFetchRequest<Contacts> = Contacts.fetchRequest()
+                    request.entity = entityDescription
+
+                    if let name = name.text {
+                        let pred = NSPredicate(format: "(name = %@)", name)
+                        request.predicate = pred
+                    }
+
+                    do {
+                        let results = try context.fetch(request as! NSFetchRequest<NSFetchRequestResult>)
+
+                        if results.count > 0 {
+                            let match = results[0] as! NSManagedObject
+
+                            name.text = match.value(forKey: "name") as? String
+                            address.text = match.value(forKey: "address") as? String
+                            phone.text = match.value(forKey: "phone") as? String
+                            status.text = "Sucess Find : \(results.count)"
+                        } else {
+                            status.text = "Fail Find"
+                        }
+                    } catch let error {
+                        status.text = error.localizedDescription
+                    }
+
+                }
     }
 }
-
