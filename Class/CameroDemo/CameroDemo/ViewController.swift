7daf9d50839687d8865f836e23343a7c930bd921
Minyoung Lee <mminy62@gmail.com>
2023-11-24 14:14:26 +0900

    카메라 예제
    - 카메라, 앨범 등 가능
    - 개발자 계정 있어야 카메라 가능

---
+++ Class/CameroDemo/CameroDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CameroDemo
+//
+//  Created by 이민영 on 2023/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func useCamera(_ sender: Any) {
+    }
+    @IBAction func useCameraRoll(_ sender: Any) {
+    }
+}
+
