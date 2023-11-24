db851dbc8bed102ebba141f7215b84c537dc58a0
Minyoung Lee <mminy62@gmail.com>
2023-11-24 15:11:09 +0900

    촬영을 마치거나 이미지를 선택했을 경우 호출되는 imagePickerController 를 구현

--- Class/CameroDemo/CameroDemo/ViewController.swift
+++ Class/CameroDemo/CameroDemo/ViewController.swift
@@ -16,12 +16,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     // 뒷 부분에서 필요할 Boolean 속성 선언
     var newMeida: Bool?
-
         override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view.
         }
-
     @IBAction func useCamera(_ sender: Any) {
         
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
@@ -42,10 +40,55 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
 //    // 오류가 발생하면 경고 상자를 통해 사용자에게 보고
 //    @objc func image(image: UIImage, )
-//    
-    
+//
+   
     @IBAction func useCameraRoll(_ sender: Any) {
-        
-    }
-}
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.savedPhotosAlbum) {
+                    
+                    let imagePicker = UIImagePickerController()
 
+                    imagePicker.delegate = self
+                    imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+                    imagePicker.mediaTypes = [UTType.image.identifier]
+                    imagePicker.allowsEditing = false
+
+                    self.present(imagePicker, animated: true, completion: nil)
+                    
+                    newMeida = false
+                }
+        }
+    func imagePickerController(_ picker: UIImagePickerController,
+                                   didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            // UIImagePickerController.InfoKey.mediaType
+            let mediaType = info[.mediaType] as! NSString
+            
+            self.dismiss(animated: true, completion: nil)
+            
+            if mediaType.isEqual(to: UTType.image.identifier) {
+                let image = info[.originalImage] as! UIImage
+                
+                imageView.image = image
+                
+                if (newMeida == true) {
+                    UIImageWriteToSavedPhotosAlbum(image, self, #selector(ViewController.image(image:didFinishSavingWithError:contextInfo:)), nil)
+                }
+                
+            }
+        }
+        
+        // 오류가 발생하면 경고 상자를 통해 사용자에게 보고
+        @objc func image(image: UIImage,
+                         didFinishSavingWithError error: NSErrorPointer,
+                         contextInfo: UnsafeRawPointer) {
+            if error != nil {
+                print("error")
+            }
+
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            self.dismiss(animated: true, completion: nil)
+        }
+
+}
