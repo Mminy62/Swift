fc7129da68ecc8d731cff48676dfb0e301d41b81
Minyoung Lee <mminy62@gmail.com>
2023-11-30 21:33:31 +0900

    feat: OpenWeather api 연결
    - 뉴스 navigation bar item에 나오게 설정
    - 지정된 위치의 날씨가 나옴

--- Class/SwiftAPIDemo/SwiftAPIDemo/ContentView.swift
+++ Class/SwiftAPIDemo/SwiftAPIDemo/ContentView.swift
@@ -12,9 +12,11 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var network = NewsAPI.shared
+    @StateObject var weatherNetwork = WeatherAPI.shared
     
     var body: some View {
-        NavigationStack{
+            
+        NavigationStack(){
             List{
                 ForEach(network.posts, id: \.self){ result in
                     HStack{
@@ -25,19 +27,40 @@ struct ContentView: View {
                             ProgressView()
                         }
                         .frame(width: 120, height: 80)
-                            
+                        
                         Text(result.title)
                     }
                     .padding(5)
-                    
-                    
-                    
-                    
                 }
             }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Text("News")
+                    .foregroundColor(.white) }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(trailing:
+                HStack {
+                Text("\(weatherNetwork.name ?? "")")
+                AsyncImage(url: URL(string: weatherNetwork.weatherImg ?? "")){ image in
+                    image.resizable()
+                    
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(width: 50, height: 50)
+            }
+                                
+            )
+            .toolbarBackground(.visible, for: .navigationBar)
+            .toolbarBackground(Color.orange, for: .navigationBar)
+            
+            
         }
+        
         .padding()
         .onAppear(){
+            weatherNetwork.fetchData()
             network.fetchData()
         }
     }
@@ -47,6 +70,6 @@ struct ContentView: View {
     
 }
 
-//#Preview {
-//    ContentView()
-//}
+#Preview {
+    ContentView()
+}
