399c2b02da7ee29b4cf875922f7fc3c28846a016
Minyoung Lee <mminy62@gmail.com>
2023-11-30 17:49:00 +0900

    Revert "keyignore 추가"
    This reverts commit 112d1233090dccb90dbb9e58552008b340553bc6.

--- Class/SwiftAPIDemo/SwiftAPIDemo/ContentView.swift
+++ Class/SwiftAPIDemo/SwiftAPIDemo/ContentView.swift
@@ -12,38 +12,33 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var network = NewsAPI.shared
-    @StateObject var weatherNetwork = WeatherAPI.shared
     
     var body: some View {
-        GeometryReader { geometry in
-            
-            NavigationStack(){
-                VStack{
-                    List{
-                        ForEach(network.posts, id: \.self){ result in
-                            HStack{
-                                AsyncImage(url: URL(string: result.urlToImage ?? "")){ image in
-                                    image.resizable()
-                                    
-                                } placeholder: {
-                                    ProgressView()
-                                }
-                                .frame(width: 120, height: 80)
-                                    
-                                Text(result.title)
-                            }
-                            .padding(5)
+        NavigationStack{
+            List{
+                ForEach(network.posts, id: \.self){ result in
+                    HStack{
+                        AsyncImage(url: URL(string: result.urlToImage ?? "")){ image in
+                            image.resizable()
+                            
+                        } placeholder: {
+                            ProgressView()
                         }
+                        .frame(width: 120, height: 80)
+                            
+                        Text(result.title)
                     }
+                    .padding(5)
+                    
+                    
+                    
+                    
                 }
             }
-            
         }
-        
         .padding()
         .onAppear(){
             network.fetchData()
-            weatherNetwork.fetchData()
         }
     }
         
