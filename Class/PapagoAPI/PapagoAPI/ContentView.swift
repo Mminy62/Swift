6ef084a03eed448dc1ea802617a207b72d8c84b8
Minyoung Lee <mminy62@gmail.com>
2023-12-04 20:04:44 +0900

    create: papago api 연동

---
+++ Class/PapagoAPI/PapagoAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PapagoAPI
+//
+//  Created by 이민영 on 2023/12/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Class/PapagoAPI/PapagoAPI/PapagoAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PapagoAPIApp.swift
+//  PapagoAPI
+//
+//  Created by 이민영 on 2023/12/01.
+//
+
+import SwiftUI
+
+@main
+struct PapagoAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
