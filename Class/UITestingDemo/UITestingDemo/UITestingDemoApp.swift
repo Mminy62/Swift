01a979126695753de0478e431cf5bbe8da14978d
Minyoung Lee <mminy62@gmail.com>
2023-12-18 10:08:25 +0900

    [Create] 프로젝트, 테스트, gitignore 생성

---
+++ Class/UITestingDemo/UITestingDemo/UITestingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestingDemoApp.swift
+//  UITestingDemo
+//
+//  Created by 이민영 on 2023/12/18.
+//
+
+import SwiftUI
+
+@main
+struct UITestingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
