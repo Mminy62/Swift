c0c900407692ee673fa08d0468d3da8889e5db57
Minyoung Lee <mminy62@gmail.com>
2023-12-18 11:21:39 +0900

    [Refactor] test code 리팩토링

--- Class/UITestingDemo/UITestingDemoUITests/UITestingDemoUITests.swift
+++ Class/UITestingDemo/UITestingDemoUITests/UITestingDemoUITests.swift
@@ -8,32 +8,42 @@
 import XCTest
 
 final class UITestingDemoUITests: XCTestCase {
-
+    // 전역객체 생성
+    let app = XCUIApplication()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        app.launch()
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
         // 이전 메서드에서 생성된 리소스, 인스턴스를 해제하는 코드 부분
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    
 
-    func testWelcome() throws {
+    func testWelcomeText() throws {
         // test code 작성
         // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
         
         // text안에 쓴 label이 id처럼 쓰일 수 있음
-        let welcome = app.staticTexts["Welcome!"] // welcome 텍스트 부분 가지고 오는 것
+        //let welcome = app.staticTexts["Welcome!"] // welcome 텍스트 부분 가지고 오는 것
+        // 현재 앱에 존재하는 유일한 텍스트 뷰 이기 때문에 이렇게 사용 가능
+        let welcome = app.staticTexts.element
+        
         
         XCTAssert(welcome.exists) // welcome element가 실제로 나타나고 있는지 true/false로 나타냄
+        XCTAssertEqual(welcome.label, "Welcome!")
+    }
+    
+    func testLoginButton() throws {
         
+//        let login = app.buttons["Login"]
+        let login = app.buttons.element
+        
+        XCTAssert(login.exists)
+        XCTAssertEqual(login.label, "Login!")
     }
 
     func testLaunchPerformance() throws {
---
+++ Class/ViewControllerDemo/ViewControllerDemo/ViewControllerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewControllerDemoApp.swift
+//  ViewControllerDemo
+//
+//  Created by 이민영 on 2023/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewControllerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
