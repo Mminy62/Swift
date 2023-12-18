3152b4b11f02c440fd339fe8a71a8bc49b0eb79f
Minyoung Lee <mminy62@gmail.com>
2023-12-18 11:23:30 +0900

    [Create] login model&controller 생성 - User.swift

---
+++ Class/UITestingDemo/UITestingDemo/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  UITestingDemo
+//
+//  Created by 이민영 on 2023/12/18.
+//
+
+import Foundation
+
+class User: ObservableObject {
+    @Published var isLoggedin = false
+    @Published var username = ""
+    @Published var password = ""
+    
+    func login() -> Bool {
+        guard username == "test" && password == "pass" else {
+            return false
+        }
+        
+        password = ""
+        isLoggedin = true
+        
+        return true
+    }
+}
