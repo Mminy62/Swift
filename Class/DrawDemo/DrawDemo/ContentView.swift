4bd922f7f16cb00f2cbd8b8f39c0a19d946fa2bf
Minyoung Lee <mminy62@gmail.com>
2023-12-19 11:30:26 +0900

    [Create] 커스텀 경로와 도형 그리기

---
+++ Class/DrawDemo/DrawDemo/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  DrawDemo
+//
+//  Created by 이민영 on 2023/12/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+//            Circle()
+//                .fill(.blue)
+//                .frame(width: 200, height: 200)
+//            
+//            Capsule()
+//                .stroke(lineWidth: 1.0)
+            PathView()
+            MyShape()
+
+            
+//            Ellipse()
+//                .stroke(style: StrokeStyle(lineWidth: 20, dash: [CGFloat(10), CGFloat(5), CGFloat(2)]))
+        }
+        .padding()
+    }
+}
+
+struct MyShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.minX, y: rect.maxY),
+                          control: CGPoint(x: rect.midX, y: rect.midY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.closeSubpath()
+
+        return path
+    }
+}
+
+struct PathView: View {
+    var body: some View {
+        Path { path in
+            path.move(to: CGPoint(x: 10, y:10))
+            path.addLine(to: CGPoint(x: 10, y:350))
+            path.addLine(to: CGPoint(x: 300, y:300))
+            
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ Class/DrawDemo/DrawDemo/DrawDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawDemoApp.swift
+//  DrawDemo
+//
+//  Created by 이민영 on 2023/12/19.
+//
+
+import SwiftUI
+
+@main
+struct DrawDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
