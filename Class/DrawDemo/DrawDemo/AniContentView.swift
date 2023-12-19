c0460b5828df0bc2990258852b23a69f69602983
Minyoung Lee <mminy62@gmail.com>
2023-12-19 19:13:58 +0900

    [Feat] animation 추가

--- Class/DrawDemo/DrawDemo/AniContentView.swift
+++ Class/DrawDemo/DrawDemo/AniContentView.swift
@@ -7,17 +7,20 @@
 import SwiftUI
 
 struct AniContentView: View {
-        var body: some View {
-        VStack {
-            ButtonImplicitAniView()
-            ButtonExplicitAniView()
-            CircleExplicitAniView()
-            StateBindingAniView()
+    var body: some View {
+        ScrollView {
+            VStack {
+                ButtonImplicitAniView()
+                ButtonExplicitAniView()
+                CircleExplicitAniView()
+                StateBindingAniView()
+                AutoStartAniView()
+                TransitionAniView()
+            }
         }
 
     }
 }
-
 // 애니메이션과 상태 바인딩
 struct StateBindingAniView: View {
     
@@ -109,6 +112,68 @@ struct ButtonImplicitAniView: View {
     }
 }
 
+// 자동으로 애니메이션 시작하기
+struct AutoStartAniView: View {
+
+    @State private var rotation: Double = 0
+    @State private var isSpinning: Bool = true
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(lineWidth: 2)
+                .foregroundStyle(.blue)
+                .frame(width: 360)
+
+            Image(systemName: "forward.fill")
+                .font(.largeTitle)
+                .offset(y: -180)
+                .rotationEffect(.degrees(rotation))
+                .animation(.linear(duration: 5).repeatForever(autoreverses: false), value: rotation)
+        }
+        .onAppear {
+            self.isSpinning.toggle()
+            rotation = isSpinning ? 0 : 360
+        }
+
+    }
+}
+
+struct TransitionAniView: View {
+    @State private var isButtonVisible: Bool = true
+    
+    var body: some View {
+        VStack {
+            Toggle(isOn: $isButtonVisible.animation(.linear(duration: 0.5))) {
+                Text("Show/Hide Button")
+            }
+            .padding()
+            
+            if isButtonVisible {
+                Button {
+                    
+                } label: {
+                    Text("Example Button")
+                }
+                .font(.largeTitle)
+                //                .transition(.slide)
+                //                .transition(.scale)
+                //                .transition(.move(edge: .top))
+                //                .transition(.fadeAndMove)
+                .transition(.asymmetric(insertion: .scale, removal: .slide))  // 비대칭 전환
+            }
+        }
+    }
+    
+}
+
+// 전환 결합하기
+// commbined(with:)을 이용하여 불투명도와 전환을 결합
+extension AnyTransition {
+    static var fadeAndMove: AnyTransition {
+        AnyTransition.opacity.combined(with: .move(edge: .top))
+    }
+}
 #Preview {
     AniContentView()
 }
