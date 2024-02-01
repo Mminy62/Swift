a39ae0d5bd24cc5022adbadb9bcbbc57a4c1d4cd
Minyoung Lee <mminy62@gmail.com>
2024-02-02 03:08:32 +0900

    [Feat] NavigationStack으로 back 버튼 활성화

---
+++ Project/voiceMemo/voiceMemo/Model/Path/Path.swift
@@ -0,0 +1,15 @@
+//
+//  Path.swift
+//  voiceMemo
+//
+
+import Foundation
+
+class PathModel: ObservableObject {
+    @Published var paths: [PathType]
+    
+    init(paths: [PathType] = []) {
+        self.paths = paths
+    }
+    
+}
---
+++ Project/voiceMemo/voiceMemo/Model/Path/PathType.swift
@@ -0,0 +1,13 @@
+//
+//  PathType.swift
+//  voiceMemo
+//
+
+// 쌓일 수 있는 뷰 종류 나열
+enum PathType: Hashable {
+    case homeView
+    case todoView
+    case memoView
+    // memo 작성 화면인지 아닌지 확인해야함
+    
+}
