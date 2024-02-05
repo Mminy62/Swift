70b86a9071a5047f87ad0653ef569b86b4de3d7d
Minyoung Lee <mminy62@gmail.com>
2024-02-06 00:57:08 +0900

    [Feat] Todo 생성하는 기능 추가

--- Project/voiceMemo/voiceMemo/Feature/Home/TodoList/Todo/TodoViewModel.swift
+++ Project/voiceMemo/voiceMemo/Feature/Home/TodoList/Todo/TodoViewModel.swift
@@ -5,6 +5,30 @@
 
 import Foundation
 
-class TodoViewModel {
-  
+class TodoViewModel: ObservableObject {
+    @Published var title: String
+    @Published var time: Date // picker로 선택하기 때문에 Date()
+    @Published var day: Date
+    @Published var isDisplayCalendar: Bool
+    
+    init(
+        title: String = "",
+        time: Date = Date(),
+        day: Date = Date(),
+        isDisplayCalendar: Bool = false
+    ) {
+        self.title = title
+        self.time = time
+        self.day = day
+        self.isDisplayCalendar = isDisplayCalendar
+    }
+}
+
+
+extension TodoViewModel {
+    // set 함수 생성
+    func setIsDisplayCalendar(_ isDisplay: Bool) {
+        isDisplayCalendar = isDisplay
+    }
+    
 }
