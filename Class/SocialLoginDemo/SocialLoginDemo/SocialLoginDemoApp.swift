d4d51fc1221486bd8ac0eff3fcf91691a42d6500
Minyoung Lee <mminy62@gmail.com>
2024-02-08 23:56:12 +0900

    [Init] SocialLoginDemo 시작

---
+++ Class/SocialLoginDemo/SocialLoginDemo/SocialLoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialLoginDemoApp.swift
+//  SocialLoginDemo
+//
+//  Created by 이민영 on 2024/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SocialLoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
